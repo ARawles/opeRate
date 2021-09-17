f98719f75bf3f28de2ec731b186da512b09ab765
ARawles <adamrawles@hotmail.co.uk>
2021-09-17 14:02:56 +0100

    Excluded data past 2010 and updated NN model

--- data_init.R
+++ data_init.R
@@ -2,6 +2,8 @@ library(dplyr)
 vg_sales <- readr::read_csv("./data/vgsales.csv", col_types = "dccdccddddd")
 clean_vg_sales <- vg_sales %>% 
   dplyr::filter(!is.na(Year))
+clean_vg_sales <- vg_sales %>%
+  dplyr::filter(Year <= 2010)
 tidy_vg_sales <- clean_vg_sales %>% 
   tidyr::pivot_longer(cols = c(NA_Sales, EU_Sales, JP_Sales, Other_Sales, Global_Sales),
                       names_to = c("Country", NA), values_to = "Sales", names_sep = "_"
